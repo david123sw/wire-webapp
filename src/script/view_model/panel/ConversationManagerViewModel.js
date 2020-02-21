73ba3f9d2b6f972341c3e68f00a40856297a3653
RanJun <xc2007_2007@163.com>
2020-02-21 13:41:31 +0800

    群管理

--- src/script/view_model/panel/ConversationManagerViewModel.js
+++ src/script/view_model/panel/ConversationManagerViewModel.js
@@ -11,4 +11,32 @@ export class ConversationManagerViewModel extends BasePanelViewModel {
   getElementId() {
     return 'conversation-manager';
   }
+  onInviteEvt() {
+    const confirm = this.activeConversation().confirm();
+    this.activeConversation().confirm(!confirm);
+  }
+  onInvitedEvt() {
+    const confirm = this.activeConversation().member_join_confirm();
+    this.activeConversation().member_join_confirm(!confirm);
+  }
+  onUrlInviteEvt() {
+    const url_invite = this.activeConversation().url_invite();
+    this.activeConversation().url_invite(!url_invite);
+  }
+  onAddRightEvt() {
+    const checked = this.activeConversation().add_right();
+    this.activeConversation().add_right(!checked);
+  }
+  onViewMemEvt() {
+    const checked = this.activeConversation().view_mem();
+    this.activeConversation().view_mem(!checked);
+  }
+  onViewChgMemNotifyEvt() {
+    const checked = this.activeConversation().view_chg_mem_notify();
+    this.activeConversation().view_chg_mem_notify(!checked);
+  }
+  onAddFriendEvt() {
+    const checked = this.activeConversation().add_friend();
+    this.activeConversation().add_friend(!checked);
+  }
 }
